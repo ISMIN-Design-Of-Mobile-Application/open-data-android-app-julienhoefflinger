2f8c8dc88275daa2f77cd760d05c6c4ccabc9b20
Julien HOEFFLINGER <julien.hoefflinger@etu.emse.fr>
2019-11-21 11:02:52 +0100

    Affichage de google map dans le fragment carte

---
+++ app/src/main/java/com/ismin/opendataapp/carte/MapsActivity.kt
@@ -0,0 +1,17 @@
+package com.ismin.opendataapp.carte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.ismin.opendataapp.R
+
+class MapsActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+    }
+
+}
